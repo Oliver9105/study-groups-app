2b2228bda0a23c64bc0143a668fdb272a76d3c40
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-14 02:24:36 +0300

    Set up routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import StudyGroups from './components/StudyGroups';
+import Subjects from './components/Subjects';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/study-groups" element={<StudyGroups />} />
+          <Route path="/subjects" element={<Subjects />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
