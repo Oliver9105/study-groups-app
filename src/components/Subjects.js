98d229370d783e8449e522b5dbeabbe958fdd410
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-14 02:20:43 +0300

    Add basic stucture for Subjects

---
+++ src/components/Subjects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Subjects() {
+  return (
+    <div className="container">
+      <h2>Subjects</h2>
+      {/* Subjects list and functionality will go here */}
+    </div>
+  );
+}
+
+export default Subjects;
