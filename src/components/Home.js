85f6eede6de3c38e2bf523d0491b372fc3e724fa
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-14 02:10:16 +0300

    Add Home component with welcome message

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h2>Welcome to Study Buddy</h2>
+      <p>Your companion for managing study groups and subjects.</p>
+    </div>
+  );
+}
+
+export default Home;
