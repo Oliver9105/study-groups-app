905b3de47e4ee9e6bf948a217bf2175bfc14b328
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-14 02:19:18 +0300

    Add basic structure for StudyGroup

---
+++ src/components/StudyGroups.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StudyGroups() {
+  return (
+    <div className="container">
+      <h2>Study Groups</h2>
+      {/* Study groups list and functionality will go here */}
+    </div>
+  );
+}
+
+export default StudyGroups;
