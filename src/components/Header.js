13c71fc85d066ce1fca10e1bf750882c487c7f54
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-14 11:13:39 +0300

    Added useEffect to fetch subjects and profiles from backend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
       <h1>Study Buddy</h1>
       <nav>
-        <a href="/">Home</a>
-        <a href="/profile">Profile</a>
-        <a href="/study-groups">Study Groups</a>
-        <a href="/subjects">Subjects</a>
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/profile" className="nav-link">
+              Profile
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/study-groups" className="nav-link">
+              Study Groups
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/schedule" className="nav-link">
+              Schedule
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/subjects" className="nav-link">
+              Subjects
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
