7a1be520994a0ba2c4e9c8e96ff85f1abb27e41d
Oliver Cheruiyot <olivercher1000@gmail.com>
2026-02-06 12:19:37 +0300

    refactor: Update App.js with new routing structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import StudyGroups from './components/StudyGroups';
-import Subjects from './components/Subjects';
-import './App.css';
-import NewSchedule from './components/NewSchedule';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from './context/AppContext';
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Groups from "./pages/Groups";
+import Schedule from "./pages/Schedule";
+import Profile from "./components/Profile";
+import GroupDetail from "./pages/GroupDetail";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/study-groups" element={<StudyGroups />} />
-          <Route path="/subjects" element={<Subjects />} />
-          <Route path="/schedule" element={<NewSchedule />} />
-        </Routes>
-      </div>
-    </Router>
+    <AppProvider>
+      <Router>
+        <div className="app">
+          <Navigation />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/schedule" element={<Schedule />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/group/:id" element={<GroupDetail />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AppProvider>
   );
 }
 
